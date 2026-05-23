4f8b6d7f4eacb2e3c015cab78052c57e0869b6c5
ayunirahajeng-specayun <ayunirahajeng@gmail.com>
2026-05-23 13:10:32 +0700

    Update UI assets and scenes - Add new sprite assets and update UI Manager

---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject panelLangkahAR;
+    public GameObject panelJelajah;
+
+    // BUKA HALAMAN LANGKAH AR
+    public void OpenLangkahAR()
+    {
+        mainMenu.SetActive(false);
+        panelLangkahAR.SetActive(true);
+        panelJelajah.SetActive(false);
+    }
+
+    // BUKA HALAMAN JELAJAH
+    public void OpenJelajah()
+    {
+        mainMenu.SetActive(false);
+        panelLangkahAR.SetActive(false);
+        panelJelajah.SetActive(true);
+    }
+
+    // BALIK KE MENU
+    public void BackToMenu()
+    {
+        mainMenu.SetActive(true);
+        panelLangkahAR.SetActive(false);
+        panelJelajah.SetActive(false);
+    }
+}
